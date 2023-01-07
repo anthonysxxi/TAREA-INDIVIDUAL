4eb7b6ff3ff5a3400de5c385b4ae95a57b4b4a40
anthonysxxi <2020009165@unfv.edu.pe>
2023-01-07 13:37:48 -0500

    tarea individual

---
+++ tarea individual.psc
@@ -0,0 +1,11 @@
+Algoritmo sin_titulo
+	Escribir 'ingrese un numero'
+	Leer num
+	aux<-0
+	Mientras num>aux Hacer
+		aux=num
+		Escribir 'ingrese un numero'
+		Leer num
+	Fin Mientras
+	Escribir 'programa finalizado'
+FinAlgoritmo
---
+++ tarea_indivisual.js
@@ -0,0 +1,15 @@
+/*Escriba un programa que solicite al usuario números decimales mientras que el usuario escriba números mayores al primero que se ingresó.
+ Por ejemplo: si el usuario ingresa como primer número un 3.1, y luego ingresa un 4, el programa debe solicitar un tercer número. 
+ El programa continuará solicitando valores sucesivamente mientras los valores ingresados sean mayores que 3.1, 
+ caso contrario, el programa finaliza.*/
+ 
+ var num;
+ var aux = 0;
+ num = Number(prompt("ingrese un numero decimal"));
+do{
+    aux = num;
+    num = Number(prompt("ingrese un numero decimal"));
+    
+
+}while (num>aux)
+alert('PROGRAMA FINALIZADO');
